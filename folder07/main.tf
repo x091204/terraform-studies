cb8b879c6f7fe1c0374c754d0b654a13c62265a9
x091204 <akifmuhammed009@gmail.com>
2026-04-28 16:12:36 +0530

    removed cred

--- folder07/main.tf
+++ folder07/main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "http://13.234.113.250:8200"
+  address = "<ip:8200"
   skip_child_token = true
 
   auth_login {
     path = "/auth/approle/login"
 
     parameters = {
-      role_id = "4bbd97f7-0684-924d-9dfc-45e262ed1db4"
-      secret_id = "41a950c0-c145-6fec-1fef-7ef34cf45a1c"
+      role_id = ""
+      secret_id = ""
     }
   }
 }
